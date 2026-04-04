dc8f8d7b2230d3997ca48f7b485d128cc34cee96
Alejandro Toranzo <alejandrotoranzop@gmail.com>
2026-04-04 03:47:08 -0400

    creados los módulos, servicios y controladores

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AuthModule, BooksModule],
   controllers: [AppController],
   providers: [AppService],
 })
