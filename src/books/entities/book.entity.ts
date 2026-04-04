3bd0414331370db1ee86bfeda87c83c9a2d097d4
Alejandro Toranzo <alejandrotoranzop@gmail.com>
2026-04-04 04:09:34 -0400

    Configurada entidad libros

--- src/books/entities/book.entity.ts
+++ src/books/entities/book.entity.ts
@@ -1,1 +1,27 @@
-export class Book {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+} from 'typeorm';
+
+@Entity('books')
+export class Book {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  author: string;
+
+  @Column({ nullable: true })
+  isbn: string;
+
+  @Column()
+  fileUrl: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
