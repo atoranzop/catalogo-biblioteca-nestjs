d4dd174c56d81e752c013fe8c01a17fc5e2591c5
Alejandro Toranzo <alejandrotoranzop@gmail.com>
2026-04-04 03:59:13 -0400

    Configurado Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Initial validation
+  app.useGlobalPipes(new ValidationPipe());
+
+  // Swagger Configuration
+  const config = new DocumentBuilder()
+    .setTitle('OpenLib API')
+    .setDescription('API for ebook sharing')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
